641c3937fa10dad738f377a45b3fef3c4551d593
glstone-glitch <glstone@aggies.ncat.edu>
2026-02-10 11:51:51 -0500

    Add files via upload

---
+++ Lab4_.py
@@ -0,0 +1,6 @@
+phone_number = int(input())
+area_code = phone_number // 10000000
+prefix = (phone_number // 10000) % 1000
+line_number = phone_number % 10000
+
+print(f"({area_code}) {prefix}-{line_number}")
